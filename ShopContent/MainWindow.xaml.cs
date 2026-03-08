4c813ad9f80bc4cc04de5dd9c362aa967acdf454
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-08 15:54:06 +0500

    Разработанное главное окно. Тошматов

--- ShopContent/MainWindow.xaml.cs
+++ ShopContent/MainWindow.xaml.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ShopContent
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+        public static MainWindow init;
+
+        public View.Main Main = new View.Main();
+
         public MainWindow()
         {
             InitializeComponent();
+            init = this; 
+            frame.Navigate(Main); 
         }
+
+        private void OpenIndex(object sender, MouseButtonEventArgs e) =>
+        frame.Navigate(Main); 
     }
 }
