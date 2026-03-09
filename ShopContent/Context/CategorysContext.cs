f21f9d5eb9480f50e3dde8435d7f4f15f3ffc27d
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-09 18:02:19 +0500

    азработанные контексты данных. Тошматов

---
+++ ShopContent/Context/CategorysContext.cs
@@ -0,0 +1,30 @@
+﻿using ShopContent.Modell;
+using ShopContent_Oshchepkov.Classes;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+
+namespace ShopContent.Context
+{
+    public class CategorysContext : Categorys
+    {
+        /// <summary> Метод загрузки данных из БД </summary>
+        public static ObservableCollection<CategorysContext> AllCategories()
+        {
+            ObservableCollection<CategorysContext> allCategories = new ObservableCollection<CategorysContext>();
+            SqlConnection connection;
+            SqlDataReader dataCategories = Connection.Query("SELECT * FROM [dbo].[Categories]", out connection);
+
+            while (dataCategories.Read())
+            {
+                allCategories.Add(new CategorysContext()
+                {
+                    Id = dataCategories.GetInt32(0),
+                    Name = dataCategories.GetString(1)
+                });
+            }
+
+            Connection.CloseConnection(connection);
+            return allCategories;
+        }
+    }
+}
--- ShopContent/Modell/Categorys.cs
+++ ShopContent/Modell/Categorys.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace ShopContent.Modell
 {
-    internal class Categorys : INotifyPropertyChanged
+    public class Categorys : INotifyPropertyChanged
     {
         private int id;
 
