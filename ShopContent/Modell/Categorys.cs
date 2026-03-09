5c8a4e0a44790a74405785bedae19139ae9d6c54
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-09 17:07:05 +0500

    Разработанные модели данных.  Тошматов

---
+++ ShopContent/Modell/Categorys.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent.Modell
+{
+    internal class Categorys : INotifyPropertyChanged
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ShopContent/Modell/Items.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent.Modell
+{
+    public class Items : INotifyPropertyChanged
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private double price;
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        private Categorys category;
+
+        private Categorys Category
+        {
+            get { return category; }
+            set
+            {
+                category = value;
+                OnPropertyChanged("Category");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
